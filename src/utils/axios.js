a368445a4203e4b164643ded133a63e6eba35454
Pedro Jesus Landauro Quiroz <plandauro@enotria.pe>
2023-08-24 16:09:16 -0500

    cambiando url product axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -48,8 +48,8 @@ export const endpoints = {
     search: '/api/post/search',
   },
   product: {
-    // list: '/api/product/list',
-    list: '/api/Productos/GetProductos', // nuevo fredy
+    list: '/api/product/list',
+    // list: '/api/Productos/GetProductos', // nuevo fredy
     // list: '/api/Productos/GetProductos', // Api Fredy
     details: '/api/product/details',
     search: '/api/product/search',
